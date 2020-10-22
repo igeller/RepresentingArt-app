4a552c4f2fa862af6a9dd2baf4f4ba55c9482a22
IG <ieg23@miami.edu>
2020-10-22 13:52:46 -0400

    added cors

--- src/components/CardInfo.jsx
+++ src/components/CardInfo.jsx
@@ -5,7 +5,7 @@ import React from "react";
 // import {IMAGES} from '../constants/gridGallery';
 // import 'https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js';
 // import 'https://unpkg.com/@google/model-viewer/dist/model-viewer-legacy.js';
-
+// import '@google/model-viewer';
 
 // const show3dobs = function () {
 //         console.log()
@@ -20,7 +20,7 @@ const CardInfo = function() {
     return (
         <div>
             {/*<Gallery images={IMAGES} enableImageSelection={false} onClickThumbnail={show3dobs}/>*/}
-            <model-viewer src={"https://representingart.imfast.io/punu/scene.gltf"} ios-src={'https://representingart.imfast.io/punu/Punu_People_Ceremonial_Dance_Mask.usdz'} loading="eager" poster="https://representingart.imfast.io/kulango-people-ceremonial-dance-mask/textures/KulangaMaskMesh.png" ar ar-modes="webxr scene-viewer quick-look" ar-scale="auto" camera-controls/>
+            <model-viewer crossorigin src={"https://representingart.imfast.io/punu/scene.gltf"} ios-src={'https://representingart.imfast.io/punu/Punu_People_Ceremonial_Dance_Mask.usdz'} loading="eager" poster="https://representingart.imfast.io/kulango-people-ceremonial-dance-mask/textures/KulangaMaskMesh.png" ar ar-modes="webxr scene-viewer quick-look" ar-scale="auto" camera-controls/>
             {console.log('model-viewer')}
 
         </div>
