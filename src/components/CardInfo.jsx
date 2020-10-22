67a4b876a7ff133caea8a2023a41f17981a76d75
IG <ieg23@miami.edu>
2020-10-22 13:41:30 -0400

    console-log

--- src/components/CardInfo.jsx
+++ src/components/CardInfo.jsx
@@ -21,6 +21,7 @@ const CardInfo = function() {
         <div>
             {/*<Gallery images={IMAGES} enableImageSelection={false} onClickThumbnail={show3dobs}/>*/}
             <model-viewer src={"https://representingart.imfast.io/punu/scene.gltf"} ios-src={'https://representingart.imfast.io/punu/Punu_People_Ceremonial_Dance_Mask.usdz'} loading="eager" poster="https://representingart.imfast.io/kulango-people-ceremonial-dance-mask/textures/KulangaMaskMesh.png" ar ar-modes="webxr scene-viewer quick-look" ar-scale="auto" camera-controls/>
+            {console.log('model-viewer')}
 
         </div>
     );
